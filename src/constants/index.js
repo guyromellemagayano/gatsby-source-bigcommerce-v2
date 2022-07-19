9ea3d44e89418d3313e9a135a9f4714d9ef8529a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-07-19 11:17:01 +0800

    chore: update constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@
 export const IS_DEV = process.env.NODE_ENV === "development";
 export const IS_PROD = process.env.NODE_ENV === "production";
 
-// String
+// Request
 export const REQUEST_BIGCOMMERCE_API_URL = "https://api.bigcommerce.com";
 export const REQUEST_ACCEPT_HEADER = "application/json";
 export const REQUEST_CONTENT_TYPE_HEADER = "application/json";
@@ -11,8 +11,6 @@ export const REQUEST_HEADERS = {
 	"Content-Type": REQUEST_CONTENT_TYPE_HEADER
 };
 export const REQUEST_TIMEOUT = 10000;
-
-// Numbers
 export const REQUEST_BIGCOMMERCE_API_PORT = 443;
 
 // Hooks
